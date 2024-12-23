ad1e7a7975f80710c4a7c5c46d36154474e1ae86
Kescefko <markgyuro@gmail.com>
2024-12-23 11:06:02 +0100

    add new app_configuration_key for testing custom message for customer

---
+++ app_configuration_key.tf
@@ -0,0 +1,8 @@
+resource "azurerm_app_configuration_key" "ac_key" {
+    configuration_store_id = azurerm_app_configuration.ac.id
+
+    key = "eShopWeb:Settings:NoResultsMessage"
+    value = "No order possible currently for this type."
+
+    content_type = "text/plain"
+}
