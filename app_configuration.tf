9230291a4b9d72da28aec135f67a6d93d3f19971
Kescefko <markgyuro@gmail.com>
2024-12-23 10:46:27 +0100

    make resource group and location reference dynamic

--- app_configuration.tf
+++ app_configuration.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-gyorgy-dev-gwc"
-  location = "West Europe"
+  name     = var.resource_group
+  location = var.location
 }
 
 resource "azurerm_app_configuration" "ac" {
   name                = "ac-gyorgy-eshoponweb-dev-weu"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = var.resource_group
+  location            = var.location
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "subscription_id" {
   default = "be77c9ff-1e9c-45d3-b099-d57392e17b6a"
   sensitive = true
 }
+
+variable "resource_group" {
+  default = "rg-gyorgy-dev-gwc"
+}
+
+variable "location" {
+  default = "West Europe"
+}
