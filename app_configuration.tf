80f73ce923fbd5a18c25eae72466c3a49a3e7de0
Kescefko <markgyuro@gmail.com>
2024-12-23 10:53:04 +0100

    remove rg from deployment already exists

--- app_configuration.tf
+++ app_configuration.tf
@@ -1,8 +1,3 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group
-  location = var.location
-}
-
 resource "azurerm_app_configuration" "ac" {
   name                = "ac-gyorgy-eshoponweb-dev-weu"
   resource_group_name = var.resource_group
