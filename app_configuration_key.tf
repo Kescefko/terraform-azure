2ecfe4a6891ce54440480846ec0956e69110534c
Kescefko <markgyuro@gmail.com>
2024-12-30 10:37:42 +0100

    comment out app config key for now

--- app_configuration_key.tf
+++ app_configuration_key.tf
@@ -1,8 +1,8 @@
-resource "azurerm_app_configuration_key" "ac_key" {
-    configuration_store_id = azurerm_app_configuration.ac.id
+# resource "azurerm_app_configuration_key" "ac_key" {
+#     configuration_store_id = azurerm_app_configuration.ac.id
 
-    key = "eShopWeb:Settings:NoResultsMessage"
-    value = "No order possible currently for this type."
+#     key = "eShopWeb:Settings:NoResultsMessage"
+#     value = "No order possible currently for this type."
 
-    content_type = "text/plain"
-}
+#     content_type = "text/plain"
+# }
